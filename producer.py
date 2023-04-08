ff1243fdada40a930fc6c77e3ff541f1e2bb1a7c
AL AMIN <encrypt.h@gmail.com>
2023-04-09 01:45:50 +0200

    Created Producer
    Created Producer

---
+++ producer.py
@@ -0,0 +1,16 @@
+from kafka import KafkaProducer
+import json, time
+from data import get_registered_user
+
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
+producer = KafkaProducer(bootstrap_servers=['192.168.0.187:9092'],
+                         value_serializer=json_serializer)
+
+if __name__ == "__main__":
+    while 1 == 1:
+        registered_user = get_registered_user()
+        print(registered_user)
+        producer.send("registered_user", registered_user)
+        time.sleep(4)
