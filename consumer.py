890b08a080b0e0edb9ada0ac5bc45bb66ac23a1e
AL AMIN <encrypt.h@gmail.com>
2023-04-09 01:47:15 +0200

    Created the consumer
    Created the consumer

---
+++ consumer.py
@@ -0,0 +1,14 @@
+from kafka import KafkaConsumer
+import json
+
+
+if __name__ == "__main__":
+    consumer = KafkaConsumer(
+        "registered_user",
+        bootstrap_servers='192.168.0.187:9092',
+        auto_offset_reset = 'earliest',
+        group_id = "consumer-group-a"
+    )
+    print("starting the consumer")
+    for msg in consumer:
+        print(f"Registered user = {json.loads(msg.value)}")
